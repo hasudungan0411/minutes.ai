c08195231fbb57d16a047909f10e2093c99718f9
ReendyPolibatam <rendypoltek006@gmail.com>
2024-10-17 20:09:51 +0700

    notifikasi sukses login dengan google

---
+++ resources/views/success.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesan Sukses</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-50 flex items-center justify-center min-h-screen">
+    <div class="bg-white p-8 rounded shadow-md text-center">
+        <h1 class="text-2xl font-bold mb-4">Success</h1>
+        <p class="mb-6">Anda Berhasil Login</p>
+        <button class="bg-purple-100 text-black py-2 px-4 rounded">OK</button>
+    </div>
+</body>
+</html>
